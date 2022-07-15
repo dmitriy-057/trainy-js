0edb3ef0402a83eba7c642cdff100b781d7dddbe
Dmitriy Goncharov <my.webpage057@gmail.com>
2022-07-15 22:52:40 +0300

    1

--- js/delineation-1.js
+++ js/delineation-1.js
@@ -1,26 +1,26 @@
 const tagsList = document.querySelector(".tags");
 
-// // выбираем один текущий тэг
-// tagsList.addEventListener("click", onTagsListClick);
+// выбираем один текущий тэг
+tagsList.addEventListener("click", onTagsListClick);
 
-// let selectedTag = null;
-// function onTagsListClick(e) {
-//   const nextActiveTag = e.target;
-//   if (nextActiveTag.nodeName !== "BUTTON") {
-//     return;
-//   }
+let selectedTag = null;
+function onTagsListClick(e) {
+  const nextActiveTag = e.target;
+  if (nextActiveTag.nodeName !== "BUTTON") {
+    return;
+  }
 
-//   //   console.log(e.currentTarget);
-//   const currentActiveTag = document.querySelector(".tags__btn--active");
+  //   console.log(e.currentTarget);
+  const currentActiveTag = document.querySelector(".tags__btn--active");
 
-//   if (currentActiveTag) {
-//     currentActiveTag.classList.remove("tags__btn--active");
-//   }
-//   // след строка заменяет if --> currentActiveTag?.classList.remove("tags__btn--active");
-//   nextActiveTag.classList.add("tags__btn--active");
-//   selectedTag = nextActiveTag.dataset.value;
-//   console.log(selectedTag);
-// }
+  if (currentActiveTag) {
+    currentActiveTag.classList.remove("tags__btn--active");
+  }
+  // след строка заменяет if --> currentActiveTag?.classList.remove("tags__btn--active");
+  nextActiveTag.classList.add("tags__btn--active");
+  selectedTag = nextActiveTag.dataset.value;
+  console.log(selectedTag);
+}
 
 // добавляем в контейнер выбранные теги, без их дублирования
 const tagsContainer = new Set();
